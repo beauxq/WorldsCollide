0669a2506debba0efde016c02e18779be61edf57
beauxq <beauxq@yahoo.com>
2025-03-13 18:57:53 -0700

    entry point scripts

--- build-wc-flag-metadata.py
+++ build-wc-flag-metadata.py
@@ -1,7 +1,8 @@
-def main():
-    import args
-    from metadata.flag_metadata_writer import FlagMetadataWriter
+def main() -> None:
+    from WorldsCollide import args
+    from WorldsCollide.metadata.flag_metadata_writer import FlagMetadataWriter
     FlagMetadataWriter(args).write()
+
 
 if __name__ == '__main__':
     main()
---
+++ wc.py
@@ -0,0 +1,3 @@
+if __name__ == "__main__":
+    from WorldsCollide.wc import main
+    main()
