2c6364746e46d6205235d1b9572321141db25ff6
beauxq <beauxq@yahoo.com>
2025-03-16 20:00:58 -0700

    condition lists

--- WorldsCollide/objectives/conditions/_battle_condition.py
+++ WorldsCollide/objectives/conditions/_battle_condition.py
@@ -12,7 +12,7 @@ class _Condition(_CachedFunction, asm.JSR):
         asm.JSR.__init__(self, self.address(*args, **kwargs), asm.ABS)
 
 class _BitCondition(_Condition):
-    def write(self, address: int, bit: int, true, false):
+    def write(self, address: int, bit: int, true: list | None, false: list | None):
         if true is None:
             true = []
         if false is None:
@@ -33,23 +33,23 @@ class _BitCondition(_Condition):
         return Write(Bank.F0, src, f"battle bit condition {hex(address)} {hex(bit)}")
 
 class EventBitCondition(_BitCondition):
-    def write(self, bit: int, true = None, false = None):
+    def write(self, bit: int, true: list | None = None, false: list | None = None):
         return super().write(event_bit.address(bit), event_bit.bit(bit), true, false)
 
 class BattleBitCondition(_BitCondition):
-    def write(self, bit, true = None, false = None):
+    def write(self, bit: int, true: list | None = None, false: list | None = None):
         return super().write(battle_bit.address(bit), battle_bit.bit(bit), true, false)
 
 class CharacterCondition(_BitCondition):
-    def write(self, character, true = None, false = None):
+    def write(self, character: int, true: list | None = None, false: list | None = None):
         return super().write(event_bit.address(event_bit.character_recruited(character)), character % 8, true, false)
 
 class EsperCondition(_BitCondition):
-    def write(self, esper, true = None, false = None):
+    def write(self, esper: int, true: list | None = None, false: list | None = None):
         return super().write(0x1a69 + esper // 8, esper % 8, true, false)
 
 class EventWordCondition(_Condition):
-    def write(self, word: int, count: int, ge = None, lt = None):
+    def write(self, word: int, count: int, ge: list | None = None, lt: list | None = None):
         if ge is None:
             ge = []
         if lt is None:
